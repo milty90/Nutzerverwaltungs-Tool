9bf9103aff6ed3bee04962a59322de8da8dcdc08
milantyopity <milant1990@gmail.com>
2026-01-31 11:43:19 +0100

    Refactor router configuration by removing Navigate for default route and updating path structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Root from "./routes/Root";
 import Edit from "./routes/edit/Edit";
@@ -16,12 +12,9 @@ import { ToastContainer } from "react-toastify";
 
 const router = createBrowserRouter([
   {
+    path: "/Nutzerverwaltung",
     element: <Root />,
     children: [
-      {
-        index: true,
-        element: <Navigate to="overview" replace />,
-      },
       { path: "overview", element: <Overview /> },
       { path: "create", element: <Create /> },
       { path: "edit/:id", element: <Edit /> },
